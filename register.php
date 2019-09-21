6beff708d1ee60441c93777c0c94fbe1950f4975
aliLorentz <ajosue998@gmail.com>
2019-09-21 16:22:00 -0430

    Registro

---
+++ register.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Social</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/register.css">
+</head>
+<body>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+<div class="formulario row container col-12">
+		<div class="registrar col-4">
+			<h1 >Bienvenido</h1>
+			<p class="text-center ">Ingresa a la mejor red social de la universidad experimental del tachira registrate si aun no tienes cuenta</p>
+			<a href="/" class="boton-registro">Registrarse</a>
+		</div>
+		<form class=" col-5">
+		<div class="form-group">
+			<h1 class="titulo text-center">Registro</h1>
+			<label for="exampleInputEmail1">User</label>
+			<input type="text" class="form-control" id="userInput" placeholder="Enter user">
+		<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+		</div>
+		<div class="form-group">
+			<label for="exampleInputPassword1">Password</label>
+			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		</div>
+		<button type="submit" class="btn btn-success col-12">Login</button>
+		</form>	
+	</div>
+    
+</body>
+</html>
