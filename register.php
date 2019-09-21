91cf32c4f1722942491dad7e373b6ef776775802
aliLorentz <ajosue998@gmail.com>
2019-09-21 16:34:48 -0430

    Colocando iniciar cuenta

--- register.php
+++ register.php
@@ -34,7 +34,7 @@
         <div class="registrar col-4">
 			<h1 >Bienvenido</h1>
 			<p class="text-center ">Registrate y accede a la mejor forma de comunicarte con tus compañeros de clases ¿Ya tienes cuenta?</p>
-			<a href="index.php" class="boton-registro">Registrarese</a>
+			<a href="index.php" class="boton-registro">Iniciar Cuenta</a>
 		</div>
 	</div>
     
