faa2da080768a0fb9ec15c4b9042ae69ea9316e7
Tiziana Amicarella <tizianaamicarella.tag@gmail.com>
2019-09-21 16:44:31 -0430

    Cambios en el texto

--- index.php
+++ index.php
@@ -18,7 +18,8 @@
 	<div class="formulario row container col-12">
 		<div class="registrar col-4">
 			<h1 >Bienvenido</h1>
-			<p class="text-center ">Ingresa a la mejor red social de la universidad experimental del tachira registrate si aun no tienes cuenta</p>
+			<p class="text-center ">Ingresa a la Mejor Red Social de la Universidad Experimental Nacional del Tachira</p>
+			<p class="text-center ">Registrate si aun no tienes cuenta</p>
 			<a href="/" class="boton-registro">Registrarse</a>
 		</div>
 		<form class=" col-5">
