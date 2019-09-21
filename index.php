db8bb5d7d15f2ddad801816d67d98d8a5959ab7a
aliLorentz <ajosue998@gmail.com>
2019-09-21 15:51:07 -0430

    Login terminado

--- index.php
+++ index.php
@@ -9,19 +9,30 @@
 	<link rel="stylesheet" href="assets/index.css">
 </head>
 <body>
-	<div class="formulario row">
-		<form class="container col-6">
+</br>
+</br>
+</br>
+</br>
+</br>
+
+	<div class="formulario row container col-12">
+		<div class="registrar col-4">
+			<h1 >Bienvenido</h1>
+			<p class="text-center ">Ingresa a la mejor red social de la universidad experimental del tachira registrate si aun no tienes cuenta</p>
+			<a href="/" class="boton-registro">Registrarse</a>
+		</div>
+		<form class=" col-5">
 		<div class="form-group">
-			<h1 class="text-center">Login</h1>
-			<label for="exampleInputEmail1">Email address</label>
-			<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+			<h1 class="titulo text-center">Login</h1>
+			<label for="exampleInputEmail1">User</label>
+			<input type="text" class="form-control" id="userInput" placeholder="Enter user">
 		<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 		</div>
 		<div class="form-group">
 			<label for="exampleInputPassword1">Password</label>
 			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
 		</div>
-		<button type="submit" class="btn btn-primary container col-6">Login</button>
+		<button type="submit" class="btn btn-success col-6">Login</button>
 		</form>	
 	</div>
 </body>
